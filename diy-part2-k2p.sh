fcbaac419e307ff64b809c339461bb3ed6400150
shaoyou11 <shaobinyx@gmail.com>
2020-07-06 19:33:54 +0800

    K2P

---
+++ diy-part2-k2p.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
+
+# Update Luci theme argon  
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+mkdir -p package/lean/smartdns && wget -P package/lean/smartdns https://raw.githubusercontent.com/openwrt/packages/master/net/smartdns/Makefile
+git clone https://github.com/shaoyou11/luci-app-smartdns.git package/lean/luci-app-smartdns
