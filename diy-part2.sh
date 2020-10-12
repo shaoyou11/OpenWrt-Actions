05b790250a4bbf05791825ad93190b545f790fc4
shaoyou11 <shaobinyx@gmail.com>
2020-10-12 22:22:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,9 +8,21 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
+# 修改openwrt登陆地址,把下面的192.168.12.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
-
+	
+	# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
+	#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Shaoyou11' package/lean/default-settings/files/zzz-default-settings
+	
+	# 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+sed -i "s/OpenWrt /Shaoyou11 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+	
+	# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+	
+	# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+	
 # Update Luci theme argon  
 rm -rf package/lean/luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
